bbc06118b0a0e41236215842d224800d9eb64e3d
Risperwangui <resperwangui68@gmail.com>
2023-05-24 11:14:22 -0700

    created the get opcodde file

---
+++ get.c
@@ -0,0 +1,43 @@
+#include "monty.h"
+/**
+ *_get - function that gets the opcode
+ *@line: the line that gets executed
+ *@opcode: this is the opcode
+ *@stack: this is the stack
+ *@line_number: the number of the line
+ */
+void _get(char *line, char *opcode, stack_t **stack, unsigned int line_number)
+{
+	int a = 0;
+	instruction_t table[] = {
+		{"push", push},
+		{"pint", pint},
+		{"pall", pall},
+		{"add", add},
+		{"pop", pop},
+		{"swap", swap},
+		{"nop", nop},
+		{NULL, NULL}
+	};
+
+	if (strcmp(opcode, "swap") == 0 && line_number == 1)
+	{
+		err_dir(opcode, line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	while (table[a].opcode)
+	{
+		if (strccmp(opcode, table[a].opcode) == 0)
+		{
+			table[a].f(stack, line_number);
+			return;
+		}
+		a++;
+	}
+
+	err_dir(opcode, line_number);
+	free(line);
+	free_stack(*stack);
+	exit(EXIT_FAILURE);
+}
